970058392a57d343fcbed8e7689dd9d3787105b4
kmisui <misui0724@gmial.com>
2017-07-02 23:29:37 +0900

    add function

--- sort.js
+++ sort.js
@@ -1,2 +1,13 @@
-const number = [19,3,81,1,24,21];
+let number = [19,3,81,1,24,21];
+
+let sortNumber = function (number) {
+ number.sort(function (a,b) {
+   if(a == b )
+     return 0;
+ }
+ return a < b ? -1 : 1 ;
+ });
+};
+
+sortNumber(number);
 console.log(number);
